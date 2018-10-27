e35a54b2498c44b7bb829e22c011798a4118e175
alex <boomscrip@gmail.com>
2018-10-27 17:34:42 +0300

    first deploy react proj

---
+++ build/precache-manifest.7de08ec4652a3669c7569b04ffcdacc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "ae76ca1ddf2e72855b4e",
+    "url": "/static/js/main.ae76ca1d.chunk.js"
+  },
+  {
+    "revision": "0588dc1cc410cccc03b0",
+    "url": "/static/js/1.0588dc1c.chunk.js"
+  },
+  {
+    "revision": "ae76ca1ddf2e72855b4e",
+    "url": "/static/css/main.9afa4308.chunk.css"
+  },
+  {
+    "revision": "b45a75bf02fe94fb36e0d212f8e3a094",
+    "url": "/index.html"
+  }
+];
