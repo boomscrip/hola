0c4e9efa493c712c397c18cdd8a76fcb64f38625
alex <boomscrip@gmail.com>
2018-10-27 18:08:46 +0300

    Updates

---
+++ precache-manifest.d71e51c84d31bdc87381843710a16eaf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a71fe5d4971e73dd4858",
+    "url": "/hola/static/js/runtime~main.a71fe5d4.js"
+  },
+  {
+    "revision": "ae76ca1ddf2e72855b4e",
+    "url": "/hola/static/js/main.ae76ca1d.chunk.js"
+  },
+  {
+    "revision": "0588dc1cc410cccc03b0",
+    "url": "/hola/static/js/1.0588dc1c.chunk.js"
+  },
+  {
+    "revision": "ae76ca1ddf2e72855b4e",
+    "url": "/hola/static/css/main.9afa4308.chunk.css"
+  },
+  {
+    "revision": "699b7866c5d9b4237fc8ebcd1d9db000",
+    "url": "/hola/index.html"
+  }
+];
