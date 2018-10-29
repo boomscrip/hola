6a38570ced6d94cd1549a8b9a570ce0a029f8447
alex <boomscrip@gmail.com>
2018-10-29 14:54:49 +0300

    ha

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import 'bootstrap/dist/css/bootstrap.css'
 import "./styles.css";
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
+    <div className="App card">
+
+      <h1 className="card-header">Hello CodeSandbox</h1>
       <h2>Start editing to see some magic happen!</h2>
+
     </div>
   );
 }
